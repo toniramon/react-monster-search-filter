ddc121d5c71bd2af73ca4c4952b73b1809802522
Web Development <info@toniramon.es>
2020-08-05 23:23:31 +0200

    adding styles and refactor search code

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value}, () => console.log(this.state))
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -30,7 +34,7 @@ class App extends Component {
         <h1>Monsters Rolodex</h1>
         <SearchBox
             placeholder='Search Monsters'
-            handleChange={e => this.setState({ searchField: e.target.value}, () => console.log(this.state))}
+            handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
